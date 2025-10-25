f5e7c35e98bcf0f1c32b57cadbb5a73ec9769ca4
tanzina <tn59249@yahoo.com>
2025-10-25 20:26:34 +0600

    project updated

--- event_management/settings.py
+++ event_management/settings.py
@@ -183,7 +183,10 @@ EMAIL_PORT = config("EMAIL_PORT")
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 
-FRONTEND_URL = "https://event-management-system-blue.vercel.app"
+if DEBUG:
+    FRONTEND_URL = "http://127.0.0.1:8000"
+else:
+    FRONTEND_URL = "https://event-management-system-blue.vercel.app"
 
 LOGIN_URL = "/users/sign_in/"
 LOGIN_REDIRECT_URL = "/events/dashboard/"
