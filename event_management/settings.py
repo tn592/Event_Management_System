e2f44b255f7950180bbab9fe99648b67f3fac78e
tanzina <tn59249@yahoo.com>
2025-10-25 23:06:35 +0600

    project updated

--- event_management/settings.py
+++ event_management/settings.py
@@ -180,7 +180,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
-# STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 MEDIA_URL = "/media/"
